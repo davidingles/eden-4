c5ac7a240d8d98e1df26ca17babde75e3c7a0f92
davidingles <davidingleslopez@gmail.com>
2024-01-26 12:15:45 +0100

    dinamic full nice

--- src/components/three/Troquel1p0391.jsx
+++ src/components/three/Troquel1p0391.jsx
@@ -1,103 +1,79 @@
-import { useState, useEffect, Suspense } from 'react'
+import * as THREE from 'three'
+import { Suspense, useState, useRef, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { AnimationMixer } from 'three'
-import { OrbitControls, ContactShadows, useGLTF } from '@react-three/drei'
+import { Stats, OrbitControls, Environment, useGLTF, Clone, Html, ContactShadows } from '@react-three/drei'
+import { useControls } from 'leva'
 
 const Models = [
-	// { title: 'Hammer', url: './models/hammer.glb' },
-	// { title: 'Drill', url: './models/drill.glb' },
-	// { title: 'Tape Measure', url: './models/tapeMeasure.glb' },
+	// { title: 'jamoneroMacondo2', url: './gltf/jamoneroMacondo2.glb' },
 	{ title: 'cajaSeparadorRemesh2', url: './gltf/cajaSeparadorRemesh2.glb' },
 	{ title: 'troquel4e0067_glb', url: './models/troquel4e0067_glb.glb' },
 	{ title: 'CajaSeparador', url: './gltf/CajaSeparador.glb' },
 	{ title: 'jamoneroMacondo2', url: './gltf/jamoneroMacondo2.glb' },
 	{ title: 'jamoneroMacondo', url: './gltf/jamoneroMacondo3.glb' },
 	{ title: '1p0391', url: './gltf/1p0391.glb' },
+	{ title: 'AMI', url: './ami2.glb' },
 ]
+
+function Model({ url }) {
+	const { scene } = useGLTF(url)
+	scene.traverse((node) => {
+		if (node.isMesh) {
+			node.material.roughness = 1
+		}
+	})
+	const group = useRef()
+	useFrame((state) => {
+		const t = state.clock.getElapsedTime()
+		// group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, Math.cos(t / 4) / 20 + 0.25, 0.1)
+		// group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, Math.sin(t / 8) / 10, 0.1)
+		// group.current.rotation.z = THREE.MathUtils.lerp(group.current.rotation.z, Math.sin(t / 8) / 20, 0.1)
+		group.current.position.y = -.3
+		group.current.position.y = THREE.MathUtils.lerp(group.current.position.y, (-3 + Math.sin(t)) / 40, .6)
+	})
+	return (
+		<group ref={group} dispose={null} position={[0, 2, 0]} scale={1}>
+			<Clone object={scene} castShadow receiveShadow />
+		</group>
+	)
+}
+
+function Fallback() {
+	return <Html><div>Loading...</div></Html>
+}
 
 export default function EstucheConAsas({ david }) {
 	const [title, setTitle] = useState(david)
-	const [isPaused, setIsPaused] = useState(false)
 
+	const { modelo } = useControls('Model', {
+		modelo: {
+			value: david, // valor inicial
+			options: Models.map(({ title }) => title), // opciones para seleccionar
+		},
+	})
 
 	useEffect(() => {
-		setTitle(david)
-	}, [david])
+		setTitle(modelo) // Usa el valor de modelo devuelto por useControls
+	}, [modelo])
 
 	const modelIndex = Models.findIndex((m) => m.title === title)
 	const modelUrl = modelIndex !== -1 ? Models[modelIndex].url : null
 
-	const togglePause = () => {
-		setIsPaused(!isPaused)
-	}
-
 	return (
 		<>
-			<button style={{
-				position: 'absolute',
-				top: '70%',
-				left: '70%',
-				transform: 'translate(-50%, -50%)',
-				zIndex: 1000,
-
-			}} onClick={togglePause}>{isPaused ? 'Resume' : 'Pause'}</button>
 			<Canvas camera={{ position: [0, .4, -0.6], near: .01, fov: 50 }}>
-				<pointLight position={[100, 100, 0]} intensity={55555} decay={2} />
-				<pointLight position={[-100, 100, 0]} intensity={55555} decay={2} />
-				<pointLight position={[-100, 100, 100]} intensity={11111} decay={2} />
-				<pointLight position={[100, -100, -100]} intensity={11111} decay={2} />
-				<pointLight position={[100, -100, 100]} intensity={11111} decay={2} />
-				<ambientLight intensity={2} />
-				<Suspense fallback={null}>
-					{modelUrl && <Model url={modelUrl} isPaused={isPaused} />}
+				{/* <pointLight position={[100, 100, 0]} intensity={55555} decay={2} />
+        <pointLight position={[-100, 100, 0]} intensity={55555} decay={2} />
+        <pointLight position={[-100, 100, 100]} intensity={11111} decay={2} />
+        <pointLight position={[100, -100, -100]} intensity={11111} decay={2} />
+        <pointLight position={[100, -100, 100]} intensity={11111} decay={2} /> */}
+				<ambientLight intensity={4} />
+				<Suspense fallback={<Fallback />}>
+					{modelUrl && <Model url={modelUrl} />}
 				</Suspense>
-				<OrbitControls autoRotate autoRotateSpeed={.2} />
-				<ContactShadows resolution={512} scale={.5} position={[0, -0.3, 0]} blur={2} opacity={0.2} far={1} color='#8a6246' />
+				<OrbitControls autoRotate autoRotateSpeed={.6} />
+				<ContactShadows resolution={512} scale={30} position={[0, -0.3, 0.0]} blur={.1} opacity={.5} far={10} color='#8a6246' />
 			</Canvas>
 		</>
 	)
 }
-
-function Model({ url, isPaused }) {
-	const [model, setModel] = useState(null)
-	const [mixer, setMixer] = useState(null)
-
-
-
-	useEffect(() => {
-		const loader = new GLTFLoader()
-		loader.load(url, (gltf) => {
-			gltf.scene.traverse((node) => {
-				if (node.isMesh) {
-					node.material.roughness = 1
-				}
-			})
-
-
-			setModel(gltf.scene)
-
-			const mixer = new AnimationMixer(gltf.scene)
-			setMixer(mixer)
-
-			gltf.animations.forEach((clip) => {
-				mixer.clipAction(clip).play()
-			})
-		})
-	}, [url])
-
-	useFrame((state, delta) => {
-
-		if (mixer && !isPaused) {
-			mixer.update(delta)
-		}
-	})
-
-	return model ? (
-		<primitive object={model} position={[0, 0, 0]} scale={[1, 1, 1]} />
-	) : null
-}
-
-
-
-
